ede037de4a0c91f049ea61d8af2bd7fcbd158b8c
ganeshasil <ganeshasil464@gmail.com>
2024-09-24 13:26:19 +0530

    basic operation in fs module

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 const fs = require("fs")
 
-const a =fs.readFileSync("./sample.txt","utf-8",(err,data)=> {
-
-    if (err) {
+// Reading file
+const a = fs.readFile("./sample.txt","utf-8",(err,data)=>{
+    if(err){
         return err
     }
 
-    console.log(data)
+    console.log("File readed successfully" + data)
 })
 
-console.log(a)
-console.log("I am first")
+// Writing a file 
+const data = "Writing to the file"
+
+fs.writeFile("./sample.txt",data,(err,data)=>{
+    if(err){
+        return err
+    }
+    console.log("File Writted Successfully")
+})
+
+// Adding new data to the file
+
+const data = "Appending the a new data to the file"
+fs.appendFile("./sample.txt",data,(err,data)=>{
+    if(err){
+        return err
+    }
+
+    console.log("Appending data successfully")
+})
+
+// Renaming a file name
+
+fs.rename("sample.txt","file.txt",(err,data)=>{
+    if(err){
+        return err
+    }
+
+    console.log("File name Reanamed")
+})
+
+//Deleting the file
+
+fs.unlink("index.js",(err,data)=>{
+    if(err){
+        return err
+    }
+    console.log("File deleted")
+})
