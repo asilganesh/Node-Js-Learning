655a9bd04aa76c5ac3b9ba8baa14464098653a13
ganeshasil <ganeshasil464@gmail.com>
2024-09-24 16:44:44 +0530

    http modules basic server creation

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-const eventEmitter = require("events")
+const http = require("http")
+const fs = require("fs")
+const port = 3000
+const hostname = "localhost"
+const home = fs.readFileSync("./index.html","utf-8")
 
-const myEmitter = new eventEmitter()
+const server = http.createServer((req,res) => {
+    if(req.method==='GET'){
 
-myEmitter.on("greet",(name)=>{
-    console.log("Hello"+ name)
+        res.end("it is a GET Method")
+    }
+
+    if(req.method==='POST'){
+
+        let body = ''
+        req.on('data',chunk=>{
+            body+= chunk.toString()
+        })
+        req.on('end',()=>{
+            res.end("it is post method"+body)
+        })
+    }
+    // if(req.url==="/"){
+       
+    //    return  res.end(home)
+    // }
+    
+    // if(req.url==="/about"){
+    //     return res.end("<h1>About Page</h1>")
+    // }
+
+    // else{
+    //     res.end("<h1>404 Page not found</h1>")
+    // }
 })
 
-myEmitter.emit('greet','Asil')
+
+server.listen(port,hostname,()=>{
+    console.log(`server is running on the http://${hostname}:${port}`)
+})
